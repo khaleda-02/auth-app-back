e93fc6125d7abff4a53254d0e6c313956c5e07b4
khaled <kha>
2023-06-30 23:06:22 +0300

    added cors

--- index.js
+++ index.js
@@ -2,16 +2,25 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 require('dotenv').config();
 const connectDb = require('./config/dbConnection');
-const { errorHandler } = require('./middleware/errorMiddleware')
+const cors = require('cors');
+const { errorHandler } = require('./middleware/errorMiddleware');
 const app = express();
 const port = 3001 || process.env.PORT;
 
 connectDb();
-
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  next();
+})
+
+// Routes
 app.use('/api/auth', require('./routes/authRoutes'));
+
+
 app.use(errorHandler);
 
 app.listen(port, () => {
