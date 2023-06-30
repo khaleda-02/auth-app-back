9bd54ebd28e17d487cce29d0ff11a57fb0e5ecea
khaled <kha>
2023-06-30 21:17:45 +0300

    fix env connection variable

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectDb = async () => {
   try {
     console.log('conecting')
-    const res = await mongoose.connect(process.env.DB_CONNECTION);
+    const res = await mongoose.connect(process.env.CONNECTION);
     console.log('connection successs !!')
   } catch (err) {
     console.log('connection failed !!' ,err )
