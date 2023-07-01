60e22de38ea681b0157e827dfaaf66567d70bc5e
khaled <kha>
2023-07-02 01:43:32 +0300

    update authMiddleware , old : token from cookie , now : token from client

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -2,44 +2,45 @@ const asyncHandler = require('express-async-handler');
 const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 
-//! when the token stored with the cookies case
+//! when the token stored with the client side case
 const authenticateUser = asyncHandler(async (req, res, next) => {
-  let token = req.cookies.token; // token => token without bearer
-  if (!token) {
+  let token;
+  //!getting the token
+  let header = req.headers.authorization;
+  if (header && header.startsWith('Bearer')) {
+    token = header.split(' ')[1];
+    //! validate the token
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = await User.findOne({ _id: decoded.id }).select('_id email username verified');
+    next();
+  } else {
     res.status(401);
     throw new Error('unauthorized , missing token')
   }
-
-  try {
-    const userDecoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = await User.findOne({ _id: userDecoded.id }).select('_id email username verified');
-    next();
-    //? when we sign the token , we just pass the id without ( email , username , verified ) , becase we need those field up-to-date when I visit protected routes 
-  } catch (erro) {
-    res
-      .clearCookie('token')
-      .status(401);
-    throw new Error('unauthorized , invalid token');
-  }
-
 })
 
-
-//! when the token stored with the client side case
-// const verify = asyncHandler(async (req, res, next) => {
-//   let token;
-//   //!getting the token
-//   let header = req.headers.authorization;
-//   if (header && header.startsWith('Bearer')) {
-//     token = header.split(' ')[1];
-//     //! validate the token
-//     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-//     req.user = await User.findOne({ _id: decoded.id }).select('-password');
-//     next();
-//   } else {
+//! when the token stored with the cookies case
+// const authenticateUser = asyncHandler(async (req, res, next) => {
+//   let token = req.cookies.token; // token => token without bearer
+//   if (!token) {
 //     res.status(401);
 //     throw new Error('unauthorized , missing token')
 //   }
+
+//   try {
+//     const userDecoded = jwt.verify(token, process.env.JWT_SECRET);
+//     req.user = await User.findOne({ _id: userDecoded.id }).select('_id email username verified');
+//     next();
+//     //? when we sign the token , we just pass the id without ( email , username , verified ) , becase we need those field up-to-date when I visit protected routes 
+//   } catch (erro) {
+//     res
+//       .clearCookie('token')
+//       .status(401);
+//     throw new Error('unauthorized , invalid token');
+//   }
+
 // })
+
+
 
 module.exports = authenticateUser;
